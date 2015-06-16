703ac8cfc3bb2c7026cf5d08cf75c3c239a61dbb
Kashif Jamal Soofi <kashif.soofi@gmail.com>
2015-06-16 21:59:25 +0100

    Initial Commit

---
+++ AutoDoc/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using AutoDoc.Web.Models;
+
+namespace AutoDoc.Web
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(CreateUser());
+        }
+
+        public User CreateUser()
+        {
+            User user = new User()
+            {
+                Name = "Kashif Jamal Soofi",
+                Address = "My address"
+            };
+
+            return user;
+        }
+    }
+}
